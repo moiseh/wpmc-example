083d18cf97bd32adb07acc505fe4dbc75e544e81
Moises Heberle <moiseh@gmail.com>
2022-07-24 22:00:57 -0300

    Massive code revamp

--- inc.player.list.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// example how to alter the default per-page listing rows
-add_filter('wpmc_list_per_page', function(){
-    return 4;
-}, 10, 1);
-
--- wpmc-example.php
+++ wpmc-example.php
@@ -10,132 +10,25 @@
 
 defined( 'ABSPATH' ) or die( 'Not allowed' );
 
-add_action('wpmc_entities', function($entities){
-    $entities['team'] = [
-        'table_name' => 'mc_teams',
-        'default_order' => 'name',
-        'display_field' => 'name',
-        'menu_icon' => 'dashicons-admin-multisite',
-        'singular' => 'Team',
-        'plural' => 'Teams',
-        'fields' => [
-            'name' => [
-                'label' => 'Name',
-                'type' => 'text',
-                'required' => true,
-            ],
-            'players' => [
-                'label' => 'Players',
-                'type' => 'one_to_many',
-                'ref_entity' => 'player',
-                'ref_column' => 'team_id',
-            ],
-        ]
-    ];
+add_action('admin_menu', function(){
+    add_menu_page( 'WPMC Cruds', 'WPMC Cruds', '', 'wpmc-example', null, 'dashicons-admin-multisite' );
+});
 
-    $entities['player'] = [
-        'table_name' => 'mc_players',
-        'default_order' => 'name',
-        'display_menu' => false,
-        'display_field' => 'name',
-        'menu_icon' => 'dashicons-admin-users',
-        'singular' => 'Player',
-        'plural' => 'Players',
-        'fields' => [
-            'name' => [
-                'label' => 'Name',
-                'type' => 'text',
-                'required' => true,
-            ],
-            'lastname' => [
-                'label' => 'Last name',
-                'type' => 'text',
-                'listable' => false,
-            ],
-            'email' => [
-                'label' => 'E-mail',
-                'type' => 'email',
-            ],
-            'team_id' => [
-                'label' => 'Team',
-                'type' => 'belongs_to',
-                'ref_entity' => 'team',
-                'required' => true,
-            ],
-        ]
-    ];
-
-    $entities['game'] = [
-        'table_name' => 'mc_games',
-        'default_order' => 'name',
-        'display_field' => 'name',
-        'menu_icon' => 'dashicons-controls-play',
-        'singular' => 'Game',
-        'plural' => 'Games',
-        'fields' => [
-            'name' => [
-                'label' => 'Name',
-                'type' => 'text',
-                'required' => true,
-            ],
-            'players' => [
-                'label' => 'Players',
-                'type' => 'has_many',
-                'ref_entity' => 'player',
-                'pivot_table' => 'mc_game_players',
-                'pivot_left' => 'game_id',
-                'pivot_right' => 'player_id',
-            ],
-        ]
-    ];
-
+add_filter('wpmc_load_entities', function($entities){
+    $entities['team'] = __DIR__ . '/cruds/team.json';
+    $entities['player'] = __DIR__ . '/cruds/player.json';
+    $entities['game'] = __DIR__ . '/cruds/game.json';
     return $entities;
 });
 
-// example how to check if db structure migration needs to run
-add_filter('wpmc_run_create_tables', function(){
-    $lastVersion = 13;
-    $dbVersion = get_option('wpmc_example_version');
-    
-    if ( $dbVersion != $lastVersion ) {
-        update_option('wpmc_example_version', $lastVersion);
-        return true;
+function wpmcSetPlayerTeamActionCallback(\WPMC\Action\FieldableActionRunner $runner) {
+    $entity = $runner->getRootAction()->getRootEntity();
+    $playerIds = $runner->getContextIds();
+    $teamId = $runner->getInputParam('team_id');
+
+    foreach ( $playerIds as $id ) {
+        $entity->saveDbData(['id' => $id, 'team_id' => $teamId]);
     }
 
-    return false;
-});
-
-// example how to add filters and/or actions just when viewing specific entity list or form
-add_action('wpmc_before_entity', function($entity){
-
-    // protect user from manage not allowed IDs and other policies
-    require_once 'inc.global.security.php';
-
-    switch (wpmc_current_entity()) {
-        case 'player':
-            // custom Players list behaviors
-            require_once 'inc.player.list.php';
-            
-            // custom Players list actions
-            require_once 'inc.player.actions.php';
-        break;
-    }
-});
-
-// example how to filter entities by current logged user_id
-add_filter('wpmc_entity_query', function(WPMC_Query_Builder $query, WPMC_Entity $entity){
-    switch ( wpmc_current_entity() ) {
-        case 'my_entity_1':
-        case 'my_entiti_2':
-            $query->where("{$entity->get_table()}.user_id", '=', get_current_user_id()); 
-        break;
-    }
-    
-    return $query;
-}, 10, 2);
-
-// example how to modify entity data before save form to database
-add_filter('wpmc_process_save_data', function($item){
-    // $item['user_id'] = get_current_user_id();
-    return $item;
-});
+    return sprintf(__('Team defined for: %s player(s)'), $runner->countIds());
+}
